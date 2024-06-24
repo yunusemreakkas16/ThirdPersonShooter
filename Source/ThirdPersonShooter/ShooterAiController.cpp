4dd3425ef236797195028420cbbfeff7db99b6c7
Yunus Emre Akkaş <Y.Emre_Akkas@outlook.com>
2024-06-24 14:14:55 +0300

    A key has been added to the Blackboard.

--- Source/ThirdPersonShooter/ShooterAiController.cpp
+++ Source/ThirdPersonShooter/ShooterAiController.cpp
@@ -3,18 +3,24 @@
 
 #include "ShooterAiController.h"
 #include "Kismet/GameplayStatics.h"
-
+#include "BehaviorTree/BlackboardComponent.h"
 
 void AShooterAiController::BeginPlay()
 {
     Super::BeginPlay();
 
     if(AIBehavior != nullptr){RunBehaviorTree(AIBehavior);}
+
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+    GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+
 }
 
 void AShooterAiController::Tick(float DeltaSeconds)
 {
     Super::Tick(DeltaSeconds);
+
     /* APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
     
     if(LineOfSightTo(PlayerPawn))
